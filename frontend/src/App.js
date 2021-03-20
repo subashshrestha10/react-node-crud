7ae241e8073e72b758cb1b9746d585b8c1c6e82f
Subash Shrestha <subashhshrestha10@gmail.com>
2021-03-20 14:07:57 +0545

    Project Clone in own git setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mobile Market</h1>
+      {/*Add your code here*/}
+    </div>
+  );
+}
+
+export default App;
