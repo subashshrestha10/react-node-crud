ff0926098e980f2c26379d44a323f5a800af6c7d
Subash Shrestha <subashhshrestha10@gmail.com>
2021-03-21 12:16:21 +0545

    Added react-router-dom and make routing between cart and home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,30 @@
 import * as React from "react";
 import Navbar from "./component/navbar/Navbar";
 import Home from "./pages/home/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Cart from "./pages/cart/Cart";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
-    </>
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/cart">
+          <Cart/>
+        </Route>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- frontend/src/component/navbar/Navbar.js
+++ frontend/src/component/navbar/Navbar.js
@@ -2,16 +2,22 @@ import React from "react";
 import "./Navbar.scss";
 import logo from "../../logo.jpeg";
 import { ShoppingCartOutlined } from "@ant-design/icons";
-import { Badge } from 'antd';
+import { Badge } from "antd";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav>
       <div className="nav-container">
-        <img src={logo} alt="subash" />
-        <Badge count={5} size="small">
-          <ShoppingCartOutlined className="cursor-pointer" />
-        </Badge>
+        <div className="link-nav">
+          <img src={logo} alt="subash" className="mr-2"/>
+          <Link to="/home"><span>Home</span></Link>
+        </div>
+        <Link to="/cart">
+          <Badge count={5} size="small">
+            <ShoppingCartOutlined className="cursor-pointer" />
+          </Badge>
+        </Link>
       </div>
     </nav>
   );
